6a3f685f6b7b79ad4c55748e17dd0463a4aa7938
nab98 <55541995+nab98@users.noreply.github.com>
2019-10-20 15:53:05 +0200

    page accueil

--- www/Page_accueil.php
+++ www/Page_accueil.php
@@ -16,28 +16,25 @@ if(isset($_POST['inscription'])){
     $mdp = $_POST['mdp'];
     $place = $_POST['place'];
     if(!empty($nom) and !empty($email) and !empty($admin) and !empty($mdp) and !empty($place)){
-        $bdd->query('INSERT INTO user(nom, email, admin, mdp, place) VALUES ("'.$nom.'","'.$email.'","'.$admin.'","'.$mdp.'","'.$place.'")');
-                $return = "utilisateur creer";
-    }
-    if(!empty($nom) and !empty($email) and !empty($admin) and !empty($mdp) and !empty($place)){
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-          if(strlen($nom) <= 50){
-            $testEmail = $bdd->prepare('SELECT id FROM  user WHERE email = "'.$email.'"');
-            $testEmail->execute();
-            if($testEmail->rowCount() < 1){
-                $bdd->query('INSERT INTO user(nom, email, admin, mdp, place) VALUES ("'.$nom.'","'.$email.'","'.$admin.'","'.$mdp.'","'.$place.'")');
-                $return = "utilisateur creer";
-            }else return "adresse mail deja utilisé";
-          }else $return = "vous etes limite a 50 caracteres";
+            if(strlen($nom) <= 50){
+                $TestEmail = $bdd->prepare('SELECT id FROM user WHERE email ="'.$email.'"');
+                $return ="utilisateur créer!";
+                if($TestEmail->rowCount() < 1){
+                     $bdd->query('INSERT INTO user(nom, email,mdp) VALUES ("'.$nom.'","'.$email.'","'.$mdp.'")');
+
+                }else $return="adresse mail deja utilisé";
+            }else $return="limite a 50 caracteres";
         }else $return = "email invalide";
-    }else $return = "champ manquant";
+    }else $return = "un ou plusieurs champs manquants";
+        
 }
 //formulaire connexion
 if(isset($_POST['login'])){
     $email =$_POST['email'];
     $mdp =$_POST['mdp'];
     if(!empty($email) and !empty($mdp)){
-        $verifUser = $bdd->prepare('SELECT id FROM  user  WHERE email ="'.$email.'" and mdp ="'.$mdp.'"');
+        $verifUser = $bdd->query('SELECT id FROM  user  WHERE email ="'.$email.'" and mdp ="'.$mdp.'"');
         $userData = $verifUser->fecth();
         if($verifUser->rowCount() == 1){
             $_SESSION['login'] = $userData['id'];
@@ -53,7 +50,6 @@ if(isset($_POST['login'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Page d'Accueil</title>
-    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body> 
     <main>
@@ -61,44 +57,36 @@ if(isset($_POST['login'])){
         <a href="http://127.0.0.1/Page_utilisateur.php">Page Utilisateur</a>
    </main>
     <header><h1>Accueil</h1></header>
-
+    <?php
+        if(isset($_POST['inscription'])  and isset($return)) echo $return
+    ?>
     
     <form action="#" method="post">
 <p>
-   nom :<input type="text" name="nom" />
+   nom :<input type="text" name="nom" placeholder="votre nom" />
 </p>
 <p>
-   email :<input type="email" name="email" />
+   email :<input type="email" name="email" placeholder="votre email"/>
 </p>
 <p>
-   admin :<input type="text" name="admin" />
+   admin :<input type="text" name="admin" placeholder="votre admin"/>
 </p>
 <p>
-   mdp :<input type="password" name="mdp" />
+   mdp :<input type="password" name="mdp" placeholder="votre mdp"/>
 </p>
 <p>
-   place :<input type="text" name="place" />
+   place :<input type="text" name="place" placeholder="votre place"/>
 </p>
 
 <input type="submit" name ="inscription" value="inscription" />
-</form>
-     
-    <form action="Page_utilisateur" method="post">
+    <?php
+        if(isset($_POST['login']) and isset($return)) echo $return;
+    ?>
+    </form>
+    <form action="#" method="post">
     <input type="email" name="email">
     <input type="password" name="mdp">
     <input type="submit" name ="login" value="connexion" />
     </form>
 </body>
 </html> 
-<?php
-$bdd = new PDO('mysql:host=localhost;dbname=ppeparking;charset=utf8', 'root', '');
-try
-{
-    if($bdd = new PDO('mysql:host=localhost;dbname=ppeparking;charset=utf8', 'root', ''));
-    echo"connexion reussi";
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
-?>
