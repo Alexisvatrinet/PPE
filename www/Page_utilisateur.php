c18bc62568676be39c73ecb76345653befcc4c45
Alexis <alexis.vatrinet@hotmail.fr>
2019-10-21 18:20:33 +0200

    Historique_afficher_erreur_button

--- www/Page_utilisateur.php
+++ www/Page_utilisateur.php
@@ -15,11 +15,7 @@ if(!isset($_SESSION['login'])){
     header('location:Page_accueil.php');
 } 
  // Si on appuye sur le bouton
-$sql =$bdd->prepare('SELECT datereservation From place');
-$sql->execute(); //appelle la fonction
-while ($row = $sql->fetch(PDO::FETCH_ASSOC)){
-echo $row ['datereservation'];
-var_dump($row);}
+
 
 
 	
@@ -33,12 +29,22 @@ var_dump($row);}
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Page Utilisateur</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header><h1>Utilisateur</h1></header>
-
+<form action="#" method="Post">
 historique: <input type="submit" name ="historique" value="historique" />
+<table>
+    <td><?php if(!isset($_Post['historique'])){$sql =$bdd->prepare('SELECT datereservation From place');
+    $sql->execute();
+    while ($row = $sql->fetch(PDO::FETCH_ASSOC)){
+    echo $row ['datereservation'];
+    }
+}?></td>
+    <td></td>
+</table>
+</form>
 
 </body>
 </html>
