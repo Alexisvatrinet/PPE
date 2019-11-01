c7070a4910c926f68cc13382c80ee25836048d1e
Alexis <alexis.vatrinet@hotmail.fr>
2019-11-01 19:43:58 +0100

    Création_tableau

--- www/Page_utilisateur.php
+++ www/Page_utilisateur.php
@@ -42,7 +42,58 @@ historique: <input type="submit" name ="historique" value="historique" />
     echo $row ['datereservation'];
     }
 }?></td>
-    <td></td>
+<tr>
+    <td>Pour&nbsp;reserver&nbsp;:<input type="submit" name="reservation" value="reservation"></td>
+</tr>
+<td><?php require('date.php'); 
+          $date = new Date();
+          $year = date('Y');
+          $dates =$date->getAll($year);  ?></td>
+          <div class="periods">
+           <div class="year"><?php echo $year?></div>
+           <div class="months"><ul>
+                <?php foreach ($date->months as $id=>$m): ?> 
+                    <li><a href="#" id="linkMonth"<?php echo $id+1;?>><?php echo utf8_encode(substr(utf8_decode($m),0,3)); ?></a></li>
+                  <?php endforeach; ?>
+              </ul>
+            </div>
+            <?php $dates = current($dates); ?> 
+            <?php foreach ($dates as $m=>$days): ?>
+            <div class="months" id="months"><?php echo $m ?>
+            <table>
+                <thead>
+                    <tr>
+                        <?php foreach ($date->days as $d) : ?>
+                        <th>
+                        <?php echo substr($d,0,3);?>
+                        </th>
+                        <?php endforeach ; ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                <?php $end= end($days); foreach ($days as $d=>$w) : ?>
+                    <?php if($d == 1):?>
+                    <td colspan=""><?php echo $w-1; ?></td>
+                    <?php endif;?>
+                        <td>
+                            <div class ="relative">
+                            <div class ="day"><?php echo $d; ?></div>
+                            </div>
+                        </td>
+                        <?php if($w == 7): ?>
+                        </tr></tr>
+                        <?php endif; ?>
+                        <?php endforeach; ?>
+                        <?php if($end != 7):?>
+                    <td colspan=""><?php echo 7-$end; ?></td>
+                    <?php endif;?>
+                    </tr>       
+                </tbody>
+            </table></div>
+            <?php endforeach ; ?>
+          </div>
+          <pre><?php print_r($dates);?></pre>
 </table>
 </form>
 
